2d1641ee163063188b3c98076b35d2d525c31f37
jeremy <duchesne.pro@gmail.com>
2025-05-13 22:56:16 +0200

    test streaming

--- public/script.js
+++ public/script.js
@@ -12,10 +12,6 @@ const descEl = overlay.querySelector('p');
 const soundLoudPath = "M361.299413,341.610667 L328.014293,314.98176 C402.206933,233.906133 402.206933,109.96608 328.013013,28.8906667 L361.298133,2.26304 C447.910187,98.97536 447.908907,244.898347 361.299413,341.610667 Z M276.912853,69.77216 L243.588693,96.4309333 C283.38432,138.998613 283.38304,204.87488 243.589973,247.44256 L276.914133,274.101333 C329.118507,215.880107 329.118507,127.992107 276.912853,69.77216 Z M191.749973,0 L80.8957867,87.2292267 L0,87.2292267 L0,257.895893 L81.0208,257.895893 L191.749973,343.35424 L191.749973,0 Z";
 const soundOffPath = "M47.0849493,0 L298.668,251.583611 L304.101001,257.015597 L353.573532,306.488791 L384.435257,337.348961 L409.751616,362.666662 L379.581717,392.836561 L191.749,205.003 L191.749973,369.105851 L81.0208,283.647505 L0,283.647505 L0,112.980838 L80.8957867,112.980838 L91.433,104.688 L16.9150553,30.169894 L47.0849493,0 Z M361.298133,28.0146513 C429.037729,103.653701 443.797162,209.394226 405.578884,298.151284 L372.628394,265.201173 C396.498256,194.197542 381.626623,113.228555 328.013013,54.642278 L361.298133,28.0146513 Z M276.912853,95.5237713 C305.539387,127.448193 318.4688,168.293162 315.701304,208.275874 L266.464558,159.040303 C261.641821,146.125608 254.316511,133.919279 244.488548,123.156461 L243.588693,122.182545 L276.912853,95.5237713 Z M191.749973,25.7516113 L191.749,84.3256113 L158.969,51.5456113 L191.749973,25.7516113 Z";
 
-let videoList = [];
-let currentIndex = 0;
-let isSwitching = false;
-
 function setPauseIcon(svgPath, viewBox = "0 0 8 8") {
   pauseBtn.innerHTML = `
     <svg viewBox="${viewBox}" class="pause-icon" xmlns="http://www.w3.org/2000/svg">
@@ -31,36 +27,6 @@ function setSoundIcon(path) {
       <path d="${path}" />
     </svg>
   `;
-}
-
-function loadVideo(index) {
-  if (index < 0 || index >= videoList.length) return;
-
-  const data = videoList[index];
-  const src = data.src;
-
-  video.classList.add('fade-out');
-  video.classList.add('hidden');
-
-  setTimeout(() => {
-    titleEl.textContent = data.title;
-    descEl.textContent = data.description;
-
-    if (src.endsWith('.m3u8') && Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(src);
-      hls.attachMedia(video);
-    } else {
-      video.src = src;
-    }
-
-    video.onloadeddata = () => {
-      video.classList.remove('hidden');
-      video.classList.remove('fade-out');
-      video.play();
-      setSoundIcon(video.muted ? soundOffPath : soundLoudPath);
-    };
-  }, 300);
 }
 
 btn.addEventListener('click', () => {
@@ -143,18 +109,22 @@ video.addEventListener('touchend', (e) => {
   }
 });
 
-// 🚀 Initialisation
-async function init() {
-  try {
-    const res = await fetch('https://cdn.jsdelivr.net/gh/santorr/VideoStreaming/videos.json');
-    const data = await res.json();
-    videoList = data.reverse();
-    loadVideo(currentIndex);
+function init() {
+  const src = "/videos/video1.m3u8"; // ta vidéo en dur
+  titleEl.textContent = "Vidéo HLS test";
+  descEl.textContent = "Lecture d’un flux .m3u8 avec HLS.js";
+
+  if (src.endsWith('.m3u8') && Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(src);
+    hls.attachMedia(video);
+  } else {
+    video.src = src;
+  }
+
+  video.onloadeddata = () => {
+    video.play();
     video.muted = false;
     setSoundIcon(soundLoudPath);
-  } catch (err) {
-    console.error("Erreur de chargement des vidéos :", err);
-  }
+  };
 }
-
-init();
